5f02d7cae88d0ff96623def59736d260d4bb5c5d
Marcus Oliveira <marcus@marcuswiilo.com>
2017-04-15 16:06:56 -0300

    Implementation of data validation and accuracy
    code: Modifications to the file classify_acess.py
    
    Some new features were implemented in the algorithm. As:
    
    - Validation of data;
    - Acuraia of the model;
    - Percentage of correctness of the model.
    
    This model was very assertive close to 100% showing a tendency of the
    result to be skewed.
    
    For the next models check how the model could be improved by running
    away from the data.

--- classification/acess_to_the_site_or_not/classify_acess.py
+++ classification/acess_to_the_site_or_not/classify_acess.py
@@ -1,5 +1,18 @@
+from sklearn.naive_bayes import MultinomialNB
 from data import loading_acess
 
 X,Y = loading_acess()
-print(Y)
-print(X)
+
+model = MultinomialNB()
+model.fit(X, Y)
+
+results = model.predict(X)
+differences = results - Y
+
+hits = [d for d in differences if d ==0]
+total_of_hits = len(hits)
+total_of_elements = len(X)
+hit_rate = 100.0 * total_of_hits / total_of_elements
+
+print(total_of_hits)
+print(total_of_elements)
