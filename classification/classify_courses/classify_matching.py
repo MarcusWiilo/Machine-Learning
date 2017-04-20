08297d322230c88510129f3bd111a8bf676dd1ac
Marcus Wiilo <garagemdainovacao@wiilo.com.br>
2017-04-20 15:13:01 -0300

    Project classify Matching of Courses
    code: Processing, pre processing.
    
    In this exercise I made implementations to transform the raw data into a
    dataset ready to be implemented with a machine learning algorithm.

--- classification/classify_courses/classify_matching.py
+++ classification/classify_courses/classify_matching.py
@@ -1,4 +1,13 @@
-from data import loading_search
-X,Y = loading_search()
-print(X[0])
-print(Y[0])
+import pandas as pd
+
+df = pd.read_csv('search.csv')
+X_df = df[['home', 'search', 'logged']]
+Y_df = df['bought'] 
+
+Xdummies_df = pd.get_dummies(X_df)
+Ydummies_df = Y_df
+
+X = Xdummies_df.values
+Y = Ydummies_df.values
+
+print(X)
