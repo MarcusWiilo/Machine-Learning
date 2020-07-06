033f9659e1ea90edeeea75d06fab70fb899b3c96
MarcusWiilo <marcus@predictbox.io>
2020-07-06 14:09:23 -0300

    Upload files of past projects

---
+++ recomendation/introducao_pratica_algoritimos_recomendacao/algoritimo.py
@@ -0,0 +1,97 @@
+avaliacoesUsuario = {'Ana':
+        {'Freddy x Jason': 2.5,
+         'O Ultimato Bourne': 3.5,
+         'Star Trek': 3.0,
+         'Exterminador do Futuro': 3.5,
+         'Norbit': 2.5,
+         'Star Wars': 3.0},
+
+      'Marcos':
+        {'Freddy x Jason': 3.0,
+         'O Ultimato Bourne': 3.5,
+         'Star Trek': 1.5,
+         'Exterminador do Futuro': 5.0,
+         'Star Wars': 3.0,
+         'Norbit': 3.5},
+
+      'Pedro':
+        {'Freddy x Jason': 2.5,
+         'O Ultimato Bourne': 3.0,
+         'Exterminador do Futuro': 3.5,
+         'Star Wars': 4.0},
+
+      'Claudia':
+        {'O Ultimato Bourne': 3.5,
+         'Star Trek': 3.0,
+         'Star Wars': 4.5,
+         'Exterminador do Futuro': 4.0,
+         'Norbit': 2.5},
+
+      'Adriano':
+        {'Freddy x Jason': 3.0,
+         'O Ultimato Bourne': 4.0,
+         'Star Trek': 2.0,
+         'Exterminador do Futuro': 3.0,
+         'Star Wars': 3.0,
+         'Norbit': 2.0},
+
+      'Janaina':
+         {'Freddy x Jason': 3.0,
+          'O Ultimato Bourne': 4.0,
+          'Star Wars': 3.0,
+          'Exterminador do Futuro': 5.0,
+          'Norbit': 3.5},
+
+      'Leonardo':
+        {'O Ultimato Bourne':4.5,
+             'Norbit':1.0,
+         'Exterminador do Futuro':4.0}
+}
+
+avaliacoesFilme = {'Freddy x Jason':
+        {'Ana': 2.5,
+         'Marcos:': 3.0 ,
+         'Pedro': 2.5,
+         'Adriano': 3.0,
+         'Janaina': 3.0 },
+
+     'O Ultimato Bourne':
+        {'Ana': 3.5,
+         'Marcos': 3.5,
+         'Pedro': 3.0,
+         'Claudia': 3.5,
+         'Adriano': 4.0,
+         'Janaina': 4.0,
+         'Leonardo': 4.5 },
+
+     'Star Trek':
+        {'Ana': 3.0,
+         'Marcos:': 1.5,
+         'Claudia': 3.0,
+         'Adriano': 2.0 },
+
+     'Exterminador do Futuro':
+        {'Ana': 3.5,
+         'Marcos:': 5.0 ,
+         'Pedro': 3.5,
+         'Claudia': 4.0,
+         'Adriano': 3.0,
+         'Janaina': 5.0,
+         'Leonardo': 4.0},
+
+     'Norbit':
+        {'Ana': 2.5,
+         'Marcos:': 3.0 ,
+         'Claudia': 2.5,
+         'Adriano': 2.0,
+         'Janaina': 3.5,
+         'Leonardo': 1.0},
+
+     'Star Wars':
+        {'Ana': 3.0,
+         'Marcos:': 3.5,
+         'Pedro': 4.0,
+         'Claudia': 4.5,
+         'Adriano': 3.0,
+         'Janaina': 3.0}
+}
---
+++ recomendation/introducao_pratica_algoritimos_recomendacao/recomendacao.py
@@ -0,0 +1,1 @@
+from algoritimo import avaliacoes
